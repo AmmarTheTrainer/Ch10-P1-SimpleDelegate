de01dba815256b9bda0df319656e4405b4799870
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-09-28 17:55:29 +0500

    final commit from head office
    all the code is there

--- Ch10-P1-SimpleDelegate/Program.cs
+++ Ch10-P1-SimpleDelegate/Program.cs
@@ -6,11 +6,50 @@ using System.Threading.Tasks;
 
 namespace Ch10_P1_SimpleDelegate
 {
+    // This delegate can point to any method, taking two integers and returning an integer.
+    public delegate int BinaryOp(int x, int y);
+
+    // This class contains methods BinaryOp will point to.
+    public class SimpleMath
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int SquareNumber(int a) => a * a;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Simple Delegate Example *****\n");
+            // Create a BinaryOp delegate object that
+            // "points to" SimpleMath.Add().
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
 
+            // Invoke Add() method indirectly using delegate object.
+            //Console.WriteLine("10 + 10 is {0}", b(10, 10));
+
+            b.Invoke(12, 12);
+
+            //Console.WriteLine("10 + 10 is {0}", b(10, 10));
+
+            // Compiler error! Method does not match delegate pattern!
+            //BinaryOp b2 = new BinaryOp(SimpleMath.SquareNumber);
+
+            DisplayDelegateInfo(b);
+
+            Console.ReadLine();
+        }
+
+
+        static void DisplayDelegateInfo(Delegate delObj)
+        {
+            // Print the names of each member in the
+            // delegate's invocation list.
+            foreach (Delegate d in delObj.GetInvocationList())
+            {
+                Console.WriteLine("Method Name: {0}", d.Method);
+                Console.WriteLine("Type Name: {0}", d.Target);
+            }
         }
     }
 }
